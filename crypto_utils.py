3087d6c840c95efc9adc4f9b0fa7095b00352abe
smeenehan <smeenehan>
2018-12-31 00:18:22 -0800

    Up through first 5 sections of Set 1.

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,4 +1,18 @@
 import base64
+from collections import defaultdict
+from itertools import cycle
+from string import ascii_lowercase
+
+# fractional frequency of different letters in the English language
+letter_freqs = {
+'a':0.08167, 'b':0.01492, 'c':0.02782, 'd':0.04253, 'e':0.12702,  'f':0.02228,
+'g':0.02015, 'h':0.06094, 'i':0.06966, 'j':0.00153, 'k':0.00772, 'l':0.04025,
+'m':0.02406, 'n':0.06749, 'o':0.07507, 'p':0.01929, 'q':0.00095, 'r':0.05987,
+'s':0.06327, 't':0.09056, 'u':0.02758, 'v':0.00978, 'w':0.02360, 'x':0.00150,
+'y':0.01974, 'z':0.00074
+}
+
+single_bytes = [bytearray([x]) for x in range(256)]
 
 def hex_to_bytes(hex_string):
     return bytearray.fromhex(hex_string)
@@ -17,3 +31,42 @@ def hex_to_base64(hex_string):
 
 def base64_to_hex(base64_string):
     return bytes_to_hex(base64_to_bytes(base64_string))
+
+def XOR_bytes(bytes_1, bytes_2):
+    if len(bytes_1) >= len(bytes_2):
+        return bytearray([x^y for x, y in zip(bytes_1, cycle(bytes_2))])
+    else:
+        return bytearray([x^y for x, y in zip(cycle(bytes_1), bytes_2)])
+
+def probability_of_english(test_string):
+    """Estimate the probability that a given string is English-language
+    by comparing frequencies of ASCII letters to typical English values"""
+    test_string = test_string.replace(" ","").lower()
+    num_occurrences = defaultdict(int)
+
+    char_gen = (char for char in test_string if char in ascii_lowercase)
+    for char in char_gen:
+        num_occurrences[char] += 1
+    num_chars = sum([x for x in num_occurrences.values()])
+
+    score = len(test_string)-num_chars # penalty for non-English characters
+    for char in ascii_lowercase:
+        real_percent = num_occurrences[char]/num_chars if num_chars > 0 else 0
+        score += num_chars*abs(real_percent-letter_freqs[char])
+    return 1.0-score/len(test_string)
+
+def single_byte_XOR(cipher_bytes):
+    """XOR a given set of bytes with all possible single-byte keys, return
+    the most probable English result as a tuple:
+    (probability, plain-text bytes, single-byte key)"""
+    best_match = (0.0, None, None)
+    for byte in single_bytes:
+        plain_bytes = XOR_bytes(cipher_bytes, byte)
+        try:
+            probability = probability_of_english(plain_bytes.decode('utf-8'))
+        except UnicodeDecodeError:
+            pass
+        else:
+            if probability > best_match[0]:
+                best_match = (probability,plain_bytes,byte)
+    return best_match
--- test_set1.py
+++ test_set1.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 
 class Set1(TestCase):
 
+    # Utilities tests
     def test_byte_to_hex_conversion(self):
         hex_string = '2ef0'
         byte_array = bytearray([46, 240])
@@ -15,7 +16,48 @@ class Set1(TestCase):
         self.assertEqual(cu.base64_to_bytes(base64_string), byte_array)
         self.assertEqual(cu.bytes_to_base64(byte_array), base64_string)
 
+    # Convert hex to base 64
     def test_1(self):
         hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
         base64_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
         self.assertEqual(cu.hex_to_base64(hex_string), base64_string)
+
+    # Fixed XOR
+    def test_2(self):
+        byte_1 = cu.hex_to_bytes('1c0111001f010100061a024b53535009181c')
+        byte_2 = cu.hex_to_bytes('686974207468652062756c6c277320657965')
+        byte_result = cu.hex_to_bytes('746865206b696420646f6e277420706c6179')
+        self.assertEqual(cu.XOR_bytes(byte_1, byte_2), byte_result)
+
+    # single-byte XOR cipher
+    def test_3(self):
+        cipher = cu.hex_to_bytes('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+        plain_text = "Cooking MC's like a pound of bacon"
+        key_byte = 88
+        _, plain, key = cu.single_byte_XOR(cipher)
+        self.assertEqual(plain.decode('utf-8'), plain_text)
+        self.assertEqual(key[0], key_byte)
+
+    # detect single-character XOR
+    def test_4(self):
+        expect_plain = 'Now that the party is jumping\n'
+        best_prob, best_plain = 0, ''
+        with open('data/Set_1_4.txt', 'r') as f:
+            for line in f:
+                cipher = cu.hex_to_bytes(line)
+                prob, plain, _ = cu.single_byte_XOR(cipher)
+                if prob > best_prob:
+                    best_prob = prob
+                    best_plain = plain.decode('utf-8')
+        self.assertEqual(best_plain, expect_plain)
+
+    def test_5(self):
+        plain_bytes = bytearray("Burning 'em, if you ain't quick and nimble\n" \
+                                +'I go crazy when I hear a cymbal', 'utf-8')
+        key_bytes = bytearray('ICE', 'utf-8')
+        expected_bytes = cu.hex_to_bytes(
+            '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324' \
+            +'272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b202831652' \
+            +'86326302e27282f')
+        cipher_bytes = cu.XOR_bytes(plain_bytes, key_bytes)
+        self.assertEqual(cipher_bytes, expected_bytes)
