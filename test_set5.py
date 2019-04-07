4fc683391d162c705e029238b17a995f95fb2561
smeenehan <smeenehan>
2019-04-06 17:35:52 -0700

    Implement SRP and break using a zero key.

--- test_set5.py
+++ test_set5.py
@@ -1,4 +1,5 @@
-from hashlib import sha1
+from hashlib import sha1, sha256
+import hmac
 from random import choice
 from unittest import TestCase
 
@@ -108,6 +109,53 @@ def cbc_keygen(secret):
     m.update(secret)
     return m.digest()[:AES.block_size]
 
+def srp_scrambler(client_public, server_public):
+    """compute the scrambling integer used in the SRP protocal, using two
+    ephemeral public keys"""
+    m = sha256()
+    m.update(cu.int_to_bytes(client_public)+cu.int_to_bytes(server_public))
+    return int.from_bytes(m.digest(), byteorder='big')
+
+class SRPServer(object):
+    """Mock up a Secure Remote Password-like authentication"""
+    def __init__(self, g, N, k):
+        self.user_info = dict()
+        self.session_keys = dict()
+        self.g, self.N, self.k = g, N, k
+
+    def register_user(self, email, password):
+        salt = cu.random_bytes(8)
+        m = sha256()
+        m.update(salt+bytes(password, 'utf-8'))
+        xH = m.digest()
+        x = int.from_bytes(xH, byteorder='big')
+        verifier = ck.modexp(self.g, x, self.N)
+        self.user_info[email] = (salt, verifier)
+
+    def init_connect(self, email, client_public):
+        salt, verifier = self.user_info[email]
+        private, public = ck.gen_DH_keys(p=self.N, g=self.g)
+        public = (public+self.k*verifier)%self.N
+
+        # compute and store session key
+        scramble = srp_scrambler(client_public, public)
+        S = ck.modexp(verifier, scramble, self.N)
+        S = ck.modexp(client_public*S, private, self.N)
+        m = sha256()
+        m.update(cu.int_to_bytes(S))
+        self.session_keys[email] = m.digest()
+
+        return salt, public
+
+    def validate_session(self, email, validation):
+        try:
+            salt, _ = self.user_info[email]
+            key = self.session_keys[email]
+            expected = hmac.new(key, salt, sha256).digest()
+            return hmac.compare_digest(validation, expected)
+        except KeyError:
+            return False
+
 class Set5(TestCase):
 
     def test_DH_echo(self):
@@ -148,6 +196,57 @@ class Set5(TestCase):
         """Check that Eve was able to decrypt Alice and Bob's messages"""
         self.assertEqual(plain, mitm.last_client_msg)
         self.assertEqual(plain, mitm.last_server_msg)
+
+    def test_36(self):
+        email = 'oldirty@wu-tang-financial.com'
+        password = 'beetle-bailey-rhymes'
+        g, N, k = 2, ck.DH_P, 3
+        server = SRPServer(g, N, k)
+        server.register_user(email, password)
+
+        # initiate SRP connection
+        private, public = ck.gen_DH_keys(p=N, g=g)
+        salt, server_public = server.init_connect(email, public)
+
+        # compute session key client-side
+        scramble = srp_scrambler(public, server_public)
+        m = sha256()
+        m.update(salt+bytes(password, 'utf-8'))
+        xH = m.digest()
+        x = int.from_bytes(xH, byteorder='big')
+        S = ck.modexp(g, x, N)
+        S = ck.modexp(server_public-k*S, private+scramble*x, N)
+        m = sha256()
+        m.update(cu.int_to_bytes(S))
+        session_key = m.digest()
+
+        validation = hmac.new(session_key, salt, sha256).digest()
+        self.assertTrue(server.validate_session(email, validation))
+
+    def test_37(self):
+        email = 'oldirty@wu-tang-financial.com'
+        password = 'beetle-bailey-rhymes'
+        g, N, k = 2, ck.DH_P, 3
+        server = SRPServer(g, N, k)
+        server.register_user(email, password)
+
+        """Because SRP on the client side generates the session key multiplies
+        by the client's public key prior to modexp and hashing, passing any
+        multiple of N (including 0!) will result in the session key just being
+        the SHA256 hash of 0. Note that we don't even need to know g, N, k, the
+        password. NOTHING. Just pass (email, 0) and SHA256(0) is your key!!!"""
+        salt, _ = server.init_connect(email, 0)
+        m = sha256()
+        m.update(cu.int_to_bytes(0))
+        session_key = m.digest()
+
+        validation = hmac.new(session_key, salt, sha256).digest()
+        self.assertTrue(server.validate_session(email, validation))
+
+
+
+
+
 
 
 
